e604e67f51091262f4bf0fc947acb7ee39cb4b48
马师傅 <13754059+master-mashifu1@user.noreply.gitee.com>
2024-09-28 23:19:10 +0800

    sentinel入门

---
+++ cloud/sentinel/src/main/java/com/ma/SentinelApplication.java
@@ -0,0 +1,15 @@
+package com.ma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class SentinelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class, args);
+    }
+}
